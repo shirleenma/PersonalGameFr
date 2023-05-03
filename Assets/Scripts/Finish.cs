576d17ba5dea17cbf54c10a9cc3839e2a63f9d53
shirleenma <121971431+shirleenma@users.noreply.github.com>
2023-05-03 10:33:46 -0700

    sdsad

--- Assets/Scripts/Finish.cs
+++ Assets/Scripts/Finish.cs
@@ -8,6 +8,8 @@ public class Finish : MonoBehaviour
 {
     private AudioSource finishSound;
 
+    private bool levelCompleted = false;
+
     // Start is called before the first frame update
     private void Start()
     {
@@ -16,9 +18,10 @@ public class Finish : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.name == "Player")
+        if (collision.gameObject.name == "Player" & !levelCompleted)
         {
             finishSound.Play();
+            levelCompleted = true;
             Invoke("CompleteLevel", 1f);
         }
     }
